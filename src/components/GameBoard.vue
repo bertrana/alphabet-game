d63cca6bd86e7763a029522c8d2e7f39449fcfd5
bertrana <botediom@gmail.com>
2023-04-15 01:12:45 +0600

    feat: game logic

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+  import { reactive } from "vue";
   const boardSize = 4;
 
   function charCodeFromA(offset: number): number {
     return 'A'.charCodeAt(0) + offset;
   }
 
-  const alphabet: string[] = Array.from({ length: 26 }, (_, index) => String.fromCharCode(charCodeFromA(index)));
+  let alphabet: string[] = Array.from({ length: 26 }, (_, index) => String.fromCharCode(charCodeFromA(index)));
 
   // Ensure "A" is present at the beginning of the game
   // before starting the game as it's the first character in the alphabet
@@ -15,13 +16,38 @@
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value);
     
-  let currentGame: string[] = shuffled.slice(0, 15);
+  let currentGame: string[] = reactive(shuffled.slice(0, 15));
   currentGame.splice(Math.floor(Math.random() * 15), 0, "A");
+  let leftover = shuffled.slice(15, shuffled.length - 1);
+
+  function cellClicked(event: Event) {
+    let clickedChar = (event.target as HTMLDivElement).innerText;
+    if (clickedChar == alphabet[0]) {
+      alphabet.shift();
+      let nextChar: string | undefined = "";
+    
+      if (currentGame.includes(alphabet[0])) {
+        nextChar = leftover.shift();
+        if (!nextChar) nextChar = "empty" ; // TODO: empty cell
+      } else {
+        nextChar = alphabet[0];
+        leftover.splice(leftover.indexOf(alphabet[0]), 1);
+      }
+      currentGame.splice(currentGame.indexOf(clickedChar), 1, nextChar);
+    }
+
+  }
 </script>
 
 <template>
   <div class="game-wrapper">
-    <div v-for="char in currentGame" :key="char" class="cell">{{ char }}</div>
+    <div 
+      v-for="char in currentGame" 
+      :key="char" 
+      @click="cellClicked"
+      class="cell"
+      >{{ char }}
+    </div>
   </div>
 </template>
 
